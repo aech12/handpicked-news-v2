e437aaa5663f3891d279895058e444559e5568c1
aech12 <aech-12@hotmail.com>
2022-10-05 16:22:03 -0300

    Home page fully tested

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test'
 
-test('home page renders', async ({
+test('home page renders a title', async ({
   page,
 }) => {
   await page.goto('./')
   
   const title =  await page.locator('data-testid=h1');
-  await expect(title).toHaveValue("Home!")
+  await expect(title).toHaveText("Home!")
 })
